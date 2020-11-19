774f2b7df1ffa7193f3884bc7c6474ac002734a1
Andy James <andy@andyjames.org>
2020-11-19 17:37:45 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+//--------------------------------------------------------------------
+// Modules
+module "make_a_vm" {
+  source  = "app.terraform.io/andysdemo/make-a-vm/aws"
+  version = "1.0.2"
+
+  os = "centos"
+  size = "t2.small"
+  tag_name = "mysupercoolserver"
+  tag_owner = "ajames"
+  tag_ttl = 72
+}
